e45b7bc802e902bfe62b4ce0b2c0858f3216aae9
HeoJooyeon <starringname@naver.com>
2025-05-16 17:30:01 +0900

    ...

--- schema/exchange_rate.sql
+++ schema/exchange_rate.sql
@@ -2,4 +2,4 @@
 -- Query: SELECT * FROM weathercoin.exchange_rate
 -- Date: 2025-05-16 17:17
 */
-INSERT INTO `` (`rate_id`,`base_currency`,`target_currency`,`rate`,`rate_date`,`created_at`,`updated_at`,`deleted_at`,`deleted_yn`) VALUES (1,'USD','KRW',1394.54993600,'2025-05-16','2025-05-16 17:11:52','2025-05-16 17:11:52','2025-05-16 17:11:52','N');
+INSERT INTO weathercoin.exchange_rate (`rate_id`,`base_currency`,`target_currency`,`rate`,`rate_date`,`created_at`,`updated_at`,`deleted_at`,`deleted_yn`) VALUES (1,'USD','KRW',1394.54993600,'2025-05-16','2025-05-16 17:11:52','2025-05-16 17:11:52','2025-05-16 17:11:52','N');
